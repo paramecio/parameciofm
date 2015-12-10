ac27d16ceef20287a5081eca4f80ef513c6e9d00
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-12-10 15:51:13 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+import os
+from setuptools import setup, find_packages
+
+
+if sys.version_info < (3, 3):
+    raise NotImplementedError("Sorry, you need at least Python 3.3 to use paramecio.")
+
+#import paramecio
+
+setup(name='paramecio',
+      version='0.1.0b1',
+      description='Fast and simple Framework based in bottle and Mako.',
+      long_description='This framework is simple framework used for create web apps. Paramecio is modular and fast. By default have a module called admin that can be used for create ',
+      author='Antonio de la Rosa Caballero',
+      author_email='webmaster@web-t-sys.com',
+      url='http://paramecioproject.com/',
+      packages=['paramecio'],
+      include_package_data=True,
+      install_requires=['bottle', 'mako', 'passlib', 'bcrypt'],
+      entry_points={'console_scripts': [
+        'paramecio = paramecio.console:start',
+      ]},
+      license='GPLV2',
+      platforms = 'any',
+      classifiers=['Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLV2 License',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        ],
+     )
