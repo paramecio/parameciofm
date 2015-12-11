8df82b5fe91f85995d84427c4c0d7e816a9e02b2
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-12-11 03:47:33 +0100

    Little fix in documentation code in i18n

--- paramecio/citoplasma/check_i18n.py
+++ paramecio/citoplasma/check_i18n.py
@@ -40,7 +40,7 @@ def scandir(path):
             # Open file
             # obtain modules, keys, and default text
 
-    # Open all files in a loop with modules from dictionary created from open files, if module not exists, the file is in i18n
+    # Open all files in a loop with modules from dictionary created from open files, if module path is not specified, the file is in paramecio.i18n. With module path the language files are saved in i18n directory into the path, for example if path is modules/panel, the files are saved in modules/panel/i18n/example.py. If key option is saved then only saved lang with keys selected. Normally you only need a file by module and by default. key option is the last member of path. For example, you can create a language file for a module and use in other module, but don't extract key used in the other module language file used. 
     
     # Load array from file, if file not exists, is created
     
