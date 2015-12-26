88404d88ad6337641edf3b649bb70d1cb3ce18e3
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-12-26 19:04:59 +0100

    Little fixes in examples and sessions

--- paramecio/citoplasma/sessions.py
+++ paramecio/citoplasma/sessions.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
 from bottle import request
-from settings import config
+
+try:
+
+    from settings import config
+
+except:
+
+    class config:
+        cookie_name='paramecio_session'
+    
 
 def get_session():
-        return request.environ.get(config.cookie_name)
+    return request.environ.get(config.cookie_name)
