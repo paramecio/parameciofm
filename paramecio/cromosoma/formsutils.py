d06d38db52b638b20bb62c744ef11f51e407a405
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-07-22 04:23:53 +0200

    Now pass_values_to_form not reset the txt_error from fields

--- paramecio/cromosoma/formsutils.py
+++ paramecio/cromosoma/formsutils.py
@@ -25,7 +25,7 @@ def pass_values_to_form(post, arr_form, yes_error=True):
         if arr_form[key].field.error == None:
             arr_form[key].field.check(post[key])
         
-        arr_form[key].txt_error=""
+        #arr_form[key].txt_error=""
         
         if arr_form[key].required==True and arr_form[key].field.error==True and yes_error==True:
            arr_form[key].txt_error=arr_form[key].field.txt_error
