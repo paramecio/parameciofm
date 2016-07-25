0ac685d22316338208c9f8f6f58718920211f041
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-07-25 05:45:15 +0200

    All tests working now

--- paramecio/cromosoma/extraforms/i18nform.py
+++ paramecio/cromosoma/extraforms/i18nform.py
@@ -2,7 +2,11 @@
 
 from paramecio.cromosoma.coreforms import BaseForm
 from paramecio.citoplasma.i18n import I18n
-from paramecio.citoplasma.mtemplates import standard_t
+from paramecio.citoplasma.mtemplates import env_theme, PTemplate
+
+env=env_theme(__file__)
+
+t=PTemplate(env)
 
 class I18nForm(BaseForm):
     
@@ -16,4 +20,4 @@ class I18nForm(BaseForm):
         
         lang_selected=I18n.get_default_lang()
         
-        return standard_t.load_template('forms/i18nform.phtml', name_form=self.name, form=self.form_child, arr_i18n=I18n.dict_i18n, lang_selected=lang_selected)
+        return t.load_template('forms/i18nform.phtml', name_form=self.name, form=self.form_child, arr_i18n=I18n.dict_i18n, lang_selected=lang_selected)
--- tests/sendmailtest.py
+++ tests/sendmailtest.py
@@ -11,12 +11,12 @@ class TestFieldMethods(unittest.TestCase):
         
         self.assertTrue( s.send(config.portal_email, [config.email_test], 'This is a test', 'A message for test a simple email method', content_type='plain', attachments=[]) )
         
-        time.sleep(70)
+        s=sendmail.SendMail()
         
         self.assertTrue( s.send(config.portal_email, [config.email_test], 'This is a test', 'A message for test a simple email method in <b>html</b>', content_type='html', attachments=[]) )
         
-        time.sleep(70)
+        s=sendmail.SendMail()
         
         self.assertTrue( s.send(config.portal_email, [config.email_test], 'This is a test', 'A message for test a simple email method in <b>html</b> and attachments', content_type='html', attachments=['tests/images/image.jpg']) )
         
-        s.quit()
+        #s.quit()
