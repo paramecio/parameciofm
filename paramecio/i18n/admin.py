a9893341fae15e144795cfb86dcac71a873fda81
Antonio de la Rosa <webmaster@web-t-sys.com>
2017-05-19 23:30:47 +0200

    Fix admin.py

--- paramecio/i18n/admin.py
+++ paramecio/i18n/admin.py
@@ -6,33 +6,41 @@ I18n.l['en-US']=I18n.l.get('en-US', {})
 
 I18n.l['en-US']['admin']=I18n.l['en-US'].get('admin', {})
 
-I18n.l['en-US']['admin']['without_privileges']='Sin privilegios'
+I18n.l['en-US']['admin']['without_privileges']='Without privileges'
 
-I18n.l['en-US']['admin']['selected_privileges']='Privilegios seleccionados'
+I18n.l['en-US']['admin']['selected_privileges']='Selected privileges'
 
-I18n.l['en-US']['admin']['administrator']='Administrador'
+I18n.l['en-US']['admin']['administrator']='Administrator'
 
-I18n.l['en-US']['admin']['welcome_to_paramecio']='Bienvenido a Paramecio Framework!!!'
+I18n.l['en-US']['admin']['welcome_to_paramecio']='Welcome to Paramecio Admin!!!'
 
-I18n.l['en-US']['admin']['send_email']='Email para recuperar tu contraseña'
+I18n.l['en-US']['admin']['send_email']='Email for recovery your password'
 
-I18n.l['en-US']['admin']['send_password_email']='Tu nuevo password'
+I18n.l['en-US']['admin']['send_password_email']='Your new password'
 
-I18n.l['en-US']['admin']['welcome_to_admin_dashboard']='Bienvenido al panel de administrador'
+I18n.l['en-US']['admin']['welcome_to_admin_dashboard']='Welcome to Admin dashboard'
 
-I18n.l['en-US']['admin']['from_here_you_can_configure_your_site']='Desde aquí puedes configurar tu site'
+I18n.l['en-US']['admin']['from_here_you_can_configure_your_site']='From here you can configure your site'
 
-I18n.l['en-US']['admin']['sign_up']='Registrarse en Paramecio'
+I18n.l['en-US']['admin']['sign_up']='Paramecio Sign up'
 
-I18n.l['en-US']['admin']['login']='Login Paramecio'
+I18n.l['en-US']['admin']['login']='Paramecio Login'
 
-I18n.l['en-US']['admin']['remember_login']='¿Recordar login?'
+I18n.l['en-US']['admin']['remember_login']='Remember login?'
 
-I18n.l['en-US']['admin']['recovery_password']='¿Recuperar password?'
+I18n.l['en-US']['admin']['recovery_password']='Recovery password?'
 
-I18n.l['en-US']['admin']['remember_tries']='Recuerda que sólo tienes 3 intentos'
+I18n.l['en-US']['admin']['remember_tries']='Remember that only have 3 attempts'
 
-I18n.l['en-US']['admin']['applications']='Aplicaciones'
+I18n.l['en-US']['admin']['applications']='Applications'
+
+I18n.l['en-US']['admin']['modules']='Modules'
+
+I18n.l['en-US']['admin']['change_module_to_translate']='Change module to translate'
+
+I18n.l['en-US']['admin']['saving_translation']='Saving translation'
+
+I18n.l['en-US']['admin']['send']='Send'
 
 I18n.l['es-ES']=I18n.l.get('es-ES', {})
 
@@ -65,4 +73,12 @@ I18n.l['es-ES']['admin']['recovery_password']='¿Recuperar password?'
 I18n.l['es-ES']['admin']['remember_tries']='Recuerda que sólo tienes 3 intentos'
 
 I18n.l['es-ES']['admin']['applications']='Aplicaciones'
+
+I18n.l['es-ES']['admin']['modules']='Modulos'
+
+I18n.l['es-ES']['admin']['change_module_to_translate']='Cambiar modulo para traducir'
+
+I18n.l['es-ES']['admin']['saving_translation']='Guardando traducción'
+
+I18n.l['es-ES']['admin']['send']='Enviar'
 
